cb8fe334a3f94448cd8b5da41e0cbdce30006f37
Angela Garcia <100092195+angelargd8@users.noreply.github.com>
2026-07-25 18:07:15 -0600

    Initial commit

---
+++ AAngelaGarcia_melodia_2.py
@@ -0,0 +1,57 @@
+from music import *
+
+tempo = 120
+"""
+HN -> BLANCA
+QN -> NEGRA
+DQN -> NEGRA CON PUNTO
+EN -> CORCHEA
+"""
+
+notas = [
+    (REST, QN),
+    (A4, EN),
+    (F4, EN),
+    (G4, EN),
+    (A4, QN),
+    (F4, EN),
+
+    (F4, QN),
+    (A4, EN),
+    (F4, EN),
+    (G4, EN),
+    (A4, DQN),
+
+    (REST, QN),
+    (A4, EN),
+    (F4, EN),
+    (G4, EN),
+    (A4, QN),
+    (F4, EN),
+
+    (F4, QN),
+    (C4, EN),
+    (A4, EN),
+    (G4, EN),
+    (F4, DQN)
+]
+
+
+def playNotes(notas, tempo):
+    frase = Phrase()
+
+    for tono, duracion in notas:
+        nota = Note(tono, duracion)
+        frase.addNote(nota)
+
+    Mod.tiePitches(frase)
+
+    frase.setTempo(tempo)
+
+    return frase
+
+
+frase = playNotes(notas, tempo)
+frase.setInstrument(MARIMBA)
+
+Play.midi(frase)
---
+++ AAngelaGarcia_melodia_3.py
@@ -0,0 +1,78 @@
+from music import *
+
+tempo = 120
+"""
+HN -> BLANCA
+QN -> NEGRA
+DQN -> NEGRA CON PUNTO
+EN -> CORCHEA
+WN  -> REDONDA
+DHN -> BLANCA CON PUNTO
+"""
+
+notes = [
+
+
+    (G3, EN),
+    (B3, EN),
+    (D4, EN),
+    (B3, QN),
+    (FS4, QN),
+
+    (FS4, EN),
+    (E4, EN),
+    (D4, QN),
+    (D4, EN),
+    (E3, DQN),
+    (REST, EN),
+
+    (E3, EN),
+    (G3, EN),
+    (B3, EN),
+    (D4, EN),
+    (B3, QN),
+    (FS4, QN),
+
+
+    (FS4, EN),
+    (E4, EN),
+    (D4, QN),
+    (D4, EN),
+    (E3, DQN),
+
+]
+
+
+primeraVuelta = [
+    (REST, QN),
+    (E3, DQN),
+
+] + notes
+
+segundaVuelta = [
+    (E3, DQN),
+] + notes
+
+
+notas = primeraVuelta + segundaVuelta
+
+
+
+def playNotes(notas, tempo):
+    frase = Phrase()
+
+    for tono, duracion in notas:
+        nota = Note(tono, duracion)
+        frase.addNote(nota)
+
+    Mod.tiePitches(frase)
+
+    frase.setTempo(tempo)
+
+    return frase
+
+
+frase = playNotes(notas, tempo)
+frase.setInstrument(VIBRAPHONE)
+
+Play.midi(frase)
